4949a084ef3e3dc49048cdd053de27af00f4f09e
Gerard Gascón <ggasconmoline@gmail.com>
2026-01-17 23:42:54 +0100

    feat: new gitlab config

--- nas/gitlab/config/gitlab.rb
+++ nas/gitlab/config/gitlab.rb
@@ -1,13 +1,27 @@
-# SSH port
+# --- SSH port ---
 gitlab_rails['gitlab_shell_ssh_port'] = 2424
 
-external_url = 'https://git.gerardgascon.com'
+# --- Main URL ---
+external_url 'https://git.gerardgascon.com'
 
+# --- Disable Let's Encrypt ---
 letsencrypt['enable'] = false
 
-nginx['listen_port'] = 80
+# --- Nginx Proxy Manager Settings ---
+nginx['listen_port'] = 8888
 nginx['listen_https'] = false
 
+nginx['proxy_set_headers'] = {
+    "Host" => "$http_host_with_default",
+    "X-Forwarded-For" => "$proxy_add_x_forwarded_for",
+    "X-Forwarded-Proto" => "https",
+    "X-Forwarded-Ssl" => "on",
+}
+
+nginx['real_ip_header'] = 'X-Real-IP'
+nginx['real_ip_recursive'] = 'on'
+
+# --- GitLab SMTP ---
 gitlab_rails['smtp_enable'] = true
 gitlab_rails['smtp_address'] = "smtp.gmail.com"
 gitlab_rails['smtp_port'] = 587
